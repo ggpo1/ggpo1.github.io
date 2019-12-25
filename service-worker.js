99092e045f45f1638863561dac7653df991df72c
Vladimir <johnlevis199@gmail.com>
2019-12-25 10:37:15 +0300

    ci_deploy

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f52d18a56f7b3e3af1a796fe16d3002.js"
+  "/precache-manifest.0b11d942107569a79f3aa17cda9c549f.js"
 );
 
 self.addEventListener('message', (event) => {
