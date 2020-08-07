a37210b8bc84358fb787b94f9f47d784d7812455
ggpo1 <johnlevis199@gmail.com>
2020-08-07 13:25:46 +0300

    deploy

---
+++ precache-manifest.d0e5e96ee6328dca135673ea09b89690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5956fbefb85de61c45bfb7436e180a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "996af9bcb25c7de3b4b4",
+    "url": "/static/css/main.d173b7d2.chunk.css"
+  },
+  {
+    "revision": "7c51bba40731a0bcc010",
+    "url": "/static/js/2.b7c577af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b7c577af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996af9bcb25c7de3b4b4",
+    "url": "/static/js/main.0e146923.chunk.js"
+  },
+  {
+    "revision": "5381d3ff90545ba83806",
+    "url": "/static/js/runtime-main.6fd0aa5d.js"
+  }
+]);
